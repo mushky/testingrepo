9b34d500361180cdddeddda9fe24dd4ed89d171d
Ali Zaidi <msazaidi@gmail.com>
2015-09-24 17:22:26 -0700

    Completed ship items and login authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :shipitems, :receiveitems
   root 'welcome#index'
 
+  get 'welcome/options'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
