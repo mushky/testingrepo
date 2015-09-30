f815b7823153873fdb401c14d3296750b1b9e686
Ali Zaidi <msazaidi@gmail.com>
2015-09-29 17:22:14 -0700

    Updated Requisition Form; add show and edit views

--- app/controllers/requisitions_controller.rb
+++ app/controllers/requisitions_controller.rb
@@ -1,6 +1,6 @@
 class RequisitionsController < ApplicationController
   def index
-  	@requesitions = Requisition.all
+  	@requisitions = Requisition.all
   end
 
   def new
@@ -12,13 +12,62 @@ class RequisitionsController < ApplicationController
   end
 
   def create
-  	@requestion = Requisition.new
-  	if @requestion.save
-  	 redirect_to requistions_path
+    @requisition = Requisition.new(requisition_params)
+    if @requisition.save
+      redirect_to requisitions_path
     else
       render 'new'
     end
   end
 
-  
+
+  def edit
+    @requisition = Requisition.find(params[:id])
+  end
+
+  def update
+    @requisition = Requisition.find(params[:id])
+    if @requisition.update(requisition_params)
+      redirect_to requisitions_path
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    @requisition = Requisition.find(params[:id])
+  end
+
+  def destroy
+    @requisition = Requisition.find(params[:id])
+    @requisition.destroy
+
+    redirect_to requisitions_path
+  end
+
+  private
+    def requisition_params
+      params.require(:requisition).permit(:requestor, 
+                                          :pr_number, 
+                                          :pr_date, 
+                                          :company, 
+                                          :address, 
+                                          :phone,
+                                          :contact,
+                                          :comments,
+                                          :for_export,
+                                          :to_where,
+                                          :description_of_use,
+                                          :quantity,
+                                          :uom,
+                                          :part_number,
+                                          :description,
+                                          :unit_cost,
+                                          :total,
+                                          :req_date,
+                                          :acct_number,
+                                          :usage,
+                                          :approval,
+                                          :approval_date)
+    end  
 end
