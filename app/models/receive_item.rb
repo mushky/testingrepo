af4924892d1db1fe0f9b7a55ad70cb95322b71fd
Ali Zaidi <msazaidi@gmail.com>
2015-12-17 09:25:07 -0800

    Quick fix to ship and receive item validations

--- app/models/receive_item.rb
+++ app/models/receive_item.rb
@@ -1,5 +1,3 @@
 class ReceiveItem < ActiveRecord::Base
-	validates :part, presence: true
 	validates :date, presence: true 
-	validates :completed, presence: true
 end
--- app/models/ship_item.rb
+++ app/models/ship_item.rb
@@ -1,5 +1,3 @@
 class ShipItem < ActiveRecord::Base
-	validates :part, presence: true
 	validates :date, presence: true 
-	validates :completed, presence: true
 end
