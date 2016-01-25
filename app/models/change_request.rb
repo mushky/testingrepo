7302aaebfe2aa9c7e52691576e528a6432eb9810
Ali Zaidi <msazaidi@gmail.com>
2016-01-25 15:30:03 -0800

    Added ID search to ChangeRequest View

--- app/models/change_request.rb
+++ app/models/change_request.rb
@@ -1,2 +1,7 @@
 class ChangeRequest < ActiveRecord::Base
+  def self.search(query)
+  	ChangeRequest.where("id like ?", "%#{query}%")
+  	# where("program like ?", "%#{query}%")
+  	#ChangeRequest.where("old_id LIKE :search OR program LIKE :search OR form LIKE :search OR id LIKE :search", search: "%#{query}")
+  end
 end
