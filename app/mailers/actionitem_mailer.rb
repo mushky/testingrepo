a55f40e25bf1ec9117ca13f6278f2d28eb6223f3
Ali Zaidi <msazaidi@gmail.com>
2016-04-26 14:20:20 -0700

    Update Inventories

--- app/mailers/actionitem_mailer.rb
+++ app/mailers/actionitem_mailer.rb
@@ -1,5 +1,5 @@
 class ActionitemMailer < ApplicationMailer
-  default from: "actionitems@iaccesstech.com"
+  default from: "actionitems@gateway.com"
 
   def actionitem_email(actionitem)
     @actionitem = actionitem
