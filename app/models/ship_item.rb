c0ceff5942a65c2e08722788cfd182dbe85f5943
Ali Zaidi <msazaidi@gmail.com>
2016-11-08 14:55:42 -0800

    attempts to fix search on shipitems

--- app/models/ship_item.rb
+++ app/models/ship_item.rb
@@ -2,7 +2,7 @@ class ShipItem < ActiveRecord::Base
 	validates :date, presence: true
 	validates :completed, presence: true
 
-	def self.search(query)
-  	where("part like ?", "%#{query}%")
- 	end
+  def self.search(query)
+  	where("parts like ?", "%#{query}%")
+  end
 end
