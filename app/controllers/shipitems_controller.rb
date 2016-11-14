fe25cf0517321ceed6cf87db8a39b57a92c4df33
Ali Zaidi <msazaidi@gmail.com>
2016-11-14 14:23:30 -0800

    Fixed: Shipitems Search

--- app/controllers/shipitems_controller.rb
+++ app/controllers/shipitems_controller.rb
@@ -5,7 +5,11 @@ class ShipitemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] 
   
   def index
-  	@shipitems = ShipItem.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+    if params[:search]
+      @shipitems = ShipItem.search(params[:search]).order("created_at DESC").paginate(:page => params[:page], :per_page => 15)
+    else
+      @shipitems = ShipItem.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 15)
+    end
   end
 	
   def new
